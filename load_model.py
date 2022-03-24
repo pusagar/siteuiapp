7cc93fedc4de23a699b54618f69613363b9db636
Sagar Pungaliya <83455004+pusagar@users.noreply.github.com>
2022-03-24 16:10:28 +0530

    Add files via upload

--- load_model.py
+++ load_model.py
@@ -9,12 +9,12 @@ def get_model(model_path):
             rf = joblib.load(mh)
     except:
         print("Cannot fetch model from local downloading from drive")
-        if not 'randomforestmodel' in os.listdir('.'):
+        if not '/model/randomforestmodel.pkl' in os.listdir('.'):
             # example url: "https://drive.google.com/u/1/uc?id=18IxYOI-whucBTZmt5qTvvYgjlxleaSqO&export=download&confirm=t"
             #url = "https://drive.google.com/u/0/uc?id=1eZQFdqwAMHLsjwD2C-PyvcZlVAwmAudD&export=download&confirm=t"
             url = "https://drive.google.com/drive/folders/1j7jDx5qQc4IuWYUv8yDIFlA-dfMgmuSa?usp=sharing"
             r = requests.get(url, allow_redirects=True)
-            open(r"randomforestmodel.pkl", 'wb').write(r.content)
+            open(r"model/randomforestmodel.pkl", 'wb').write(r.content)
             del r
         with open(r"randomforestmodel.pkl", "rb") as m:
             rf = joblib.load(m)
