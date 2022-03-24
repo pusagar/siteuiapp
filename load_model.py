9e74ee55c5f0a59234264f9aacd782dafd684b30
Sagar Pungaliya <83455004+pusagar@users.noreply.github.com>
2022-03-24 16:02:47 +0530

    Add files via upload

--- load_model.py
+++ load_model.py
@@ -11,7 +11,8 @@ def get_model(model_path):
         print("Cannot fetch model from local downloading from drive")
         if not 'randomforestmodel' in os.listdir('.'):
             # example url: "https://drive.google.com/u/1/uc?id=18IxYOI-whucBTZmt5qTvvYgjlxleaSqO&export=download&confirm=t"
-            url = "https://drive.google.com/u/0/uc?id=1eZQFdqwAMHLsjwD2C-PyvcZlVAwmAudD&export=download&confirm=t"
+            #url = "https://drive.google.com/u/0/uc?id=1eZQFdqwAMHLsjwD2C-PyvcZlVAwmAudD&export=download&confirm=t"
+            url = "https://drive.google.com/drive/folders/1j7jDx5qQc4IuWYUv8yDIFlA-dfMgmuSa?usp=sharing"
             r = requests.get(url, allow_redirects=True)
             open(r"randomforestmodel.pkl", 'wb').write(r.content)
             del r
